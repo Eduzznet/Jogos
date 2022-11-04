6e5aef16b30451fb3caff05a1b98c6f385328ef4
Eduzznet <65985515+Eduzznet@users.noreply.github.com>
2022-11-04 15:56:55 -0300

    Update 1.3.1-AnimacaoCiclo.c

--- Exercicios/1.3.1-AnimacaoCiclo.c
+++ Exercicios/1.3.1-AnimacaoCiclo.c
@@ -16,13 +16,11 @@ int main (int argc, char* args[])
 
     /* EXECUÇÃO */
     int i=0;
-    int x,y,z;
     double j;
     int raio=100;
     int circ=2*PI*raio;
     int cx=200;
     int cy=200;
-    int dy=cy;
     SDL_Rect r = { cx+raio,cy, 10,10 };
     while (i < 2) {
         for(j=0;j<=(2*PI);j+=(PI/circ)){    
@@ -31,10 +29,8 @@ int main (int argc, char* args[])
             SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
             SDL_RenderFillRect(ren, &r);
             SDL_RenderPresent(ren);
-            x=raio*cos(j);
-            y=raio*sin(j);
-            r.x=x+cx;
-            r.y=y+cy;
+            r.x=raio*cos(j)+cx;
+            r.y=raio*sin(j)+cy;
             SDL_Delay(50);
         }
         i++;
