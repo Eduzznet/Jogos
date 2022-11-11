0b9755389677a3d6fd9b37937bac0183a8b406c0
Eduzznet <65985515+Eduzznet@users.noreply.github.com>
2022-11-11 14:16:41 -0300

    Create 1.5.3-AnimacaoCicloEVT.c

---
+++ Exercicios/1.5.3-AnimacaoCicloEVT.c
@@ -0,0 +1,79 @@
+#include <SDL2/SDL.h>
+#include <math.h>
+
+#define PI 3.1416
+
+int AUX_WaitEventTimeoutCount (SDL_Event* evt, Uint32* ms){
+    Uint32 antes = SDL_GetTicks();
+    Uint32 tempassado = 0; 
+    int isevt = SDL_WaitEventTimeout(evt, *ms);
+    if (isevt) {
+        tempassado = SDL_GetTicks()-antes;
+        if (tempassado > *ms) tempassado = *ms;
+  	    *ms -= tempassado;
+    }
+    return isevt;	
+}
+
+int main (int argc, char* args[])
+{
+    /* INICIALIZACAO */
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* win = SDL_CreateWindow("Animacao Simples",
+                         SDL_WINDOWPOS_UNDEFINED,
+                         SDL_WINDOWPOS_UNDEFINED,
+                         400, 400, SDL_WINDOW_SHOWN
+                      );
+    SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
+
+    /* EXECUÇÃO */
+    double j=0;
+    int raio=100;
+    int circ=2*PI*raio;
+    int cx=200;
+    int cy=200;
+    int es=50;
+    int passo=5;
+    int gira=1;
+    int continua=1;
+    Uint32 espera = es;
+    SDL_Rect r = { cx+raio,cy, 10,10 };
+    while (continua) {
+        SDL_Event evt;
+        int aux = AUX_WaitEventTimeoutCount(&evt, &espera);
+        if (aux) {
+            switch(evt.type){
+                case SDL_KEYDOWN: 
+                    gira= !gira;
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    passo = -passo;
+                    break;
+                //Trata os eventos do AltF4 e de fechar a janela no 'X'
+                case SDL_WINDOWEVENT: 
+                    if (evt.window.event == SDL_WINDOWEVENT_CLOSE)
+                        continua=0;
+                    break;
+            }       
+        }
+        else {
+            if(gira){
+                espera = es;               
+                SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+                SDL_RenderClear(ren);
+                SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+                SDL_RenderFillRect(ren, &r);
+                SDL_RenderPresent(ren);
+                r.x=raio*cos(j)+cx;
+                r.y=raio*sin(j)+cy;
+                j+=(PI/circ)*passo;
+                if (j>(2*PI)) j=0;
+            }
+        }
+    }
+    /* FINALIZACAO */
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    return 0;
+}
