ecf71e4c2f3c239e00feba93144c1ed9c8b6a408
Eduzznet <65985515+Eduzznet@users.noreply.github.com>
2023-02-02 13:48:49 -0300

    Update 02/02
    Implementei a biblioteca SDL_TTF para rendereizar textos na tela. Criei a função render_text para facilitar na hora de colocar mais textos. Fiz o sistema de pontuação, e de combo. Ajeitei a colisão dos asteroids, para ser com a terra agora e nao com as bordas da janela. E o 'ESC' agora volta para o menu quando o jogo estiver jogando

--- Projeto Final/main.c
+++ Projeto Final/main.c
@@ -1,10 +1,12 @@
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <stdbool.h>
+#include <SDL2/SDL_ttf.h>
 
 int MAXENT = 20;
 
@@ -60,6 +62,25 @@ int PointInRect (SDL_Point* p, SDL_Rect* r){
     p->y > r->y+r->h );
 }
 
+void render_text(
+    SDL_Renderer *renderer,
+    const char *text,
+    TTF_Font *font,
+    SDL_Rect *rect,
+    SDL_Color *color
+) {
+    SDL_Surface *surface;
+    SDL_Texture *texture;
+
+    surface = TTF_RenderText_Blended(font, text, *color);
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    //rect->w = surface->w;
+    //rect->h = surface->h;
+    SDL_FreeSurface(surface);
+    SDL_RenderCopy(renderer, texture, NULL, rect);
+    SDL_DestroyTexture(texture);
+}
+
 typedef enum {
     MENU,
     RUNNING,
@@ -81,21 +102,22 @@ int main (int argc, char* args[])
     /* INICIALIZACAO */
     SDL_Init(SDL_INIT_EVERYTHING);
     IMG_Init(0);
+    TTF_Init();
     int wh = 600;
     int ww = 600;
     int es = 100;
-    SDL_Window* win = SDL_CreateWindow("Animando e Movendo um Retângulo",
+    SDL_Window* win = SDL_CreateWindow("Projeto",
                          SDL_WINDOWPOS_UNDEFINED,
                          SDL_WINDOWPOS_UNDEFINED,
                          ww, wh, SDL_WINDOW_SHOWN
                       );
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
-    SDL_Texture* img = IMG_LoadTexture(ren, "aim.png");
-    SDL_Texture* bg = IMG_LoadTexture(ren, "space.png");
-    SDL_Texture* ast = IMG_LoadTexture(ren, "asteroid.png");
-    SDL_Texture* mn = IMG_LoadTexture(ren, "menu.png");
-    SDL_Texture* bp = IMG_LoadTexture(ren, "play.png");
-    SDL_Texture* bq = IMG_LoadTexture(ren, "quit.png");
+    SDL_Texture* img = IMG_LoadTexture(ren, "img/aim.png");
+    SDL_Texture* bg = IMG_LoadTexture(ren, "img/space.png");
+    SDL_Texture* ast = IMG_LoadTexture(ren, "img/asteroid.png");
+    SDL_Texture* mn = IMG_LoadTexture(ren, "img/menu.png");
+    SDL_Texture* bp = IMG_LoadTexture(ren, "img/play.png");
+    SDL_Texture* bq = IMG_LoadTexture(ren, "img/quit.png");
     assert(img != NULL);
     assert(bg != NULL);
     assert(ast != NULL);
@@ -103,21 +125,49 @@ int main (int argc, char* args[])
     assert(bp != NULL);
     assert(bq != NULL);
 
+    TTF_Font* fnt = TTF_OpenFont("img/LCDM2N__.ttf", 40);
+    assert(fnt != NULL);
+    SDL_Color clr = {0x00,0xFF,0x00,0xFF};
+    //char PontosString[128];
+    /*SDL_Surface* sfc1;
+    SDL_Surface* sfc2;
+    SDL_Surface* sfc3;
+    SDL_Texture* score;
+    SDL_Texture* texX;
+    SDL_Texture* combo;*/
+    //int Points=0;
+    /*sprintf(PontosString, "%d", Points);
+    sfc = TTF_RenderText_Solid(fnt, PontosString, clr);
+    assert(sfc != NULL);
+    SDL_Texture* score = SDL_CreateTextureFromSurface(ren, sfc);
+    assert(score != NULL);
+    SDL_FreeSurface(sfc);*/
+
     /* EXECUÇÃO */
     time_t t;
     srand((unsigned) time(&t));
     SDL_Rect Rects [MAXENT];// = { 40,20, 10,10,  60,300, 30,30,  20,500, 10,10 };
     //Rects[4] = teste;
+    char PontosString[128];
+    int Points=0;
     int entcout = 0;
     int gen;
     int i = 0;
     int cooldown = 0;
-    int entrespawn =0;
+    int entrespawn = 0 ;
+    int combocount = 0;
+    char comboString[128];
+    int hit = 0;
+    SDL_Rect comboRect = {27,40, 20,20};
     SDL_Rect fundoM = {0,0, ww, wh}; 
     SDL_Rect fundo = {0,0, ww, wh};
     SDL_Rect Mira = { 250,100, 0,0 };
+    SDL_Rect recorteMira;
     SDL_Rect BotaoStart = {150, 100, 300, 50};
     SDL_Rect BotaoQuit = {150, 200, 300, 50};
+    SDL_Rect Terra = {0,wh-75, ww, 200};
+    SDL_Rect ScoreRect = {5,5, 30,30};
+    SDL_Rect xRect ={10,45, 15,15};
     //SDL_Rect RGrande = {10,rand() % 500, 30,30};
     //SDL_Rect RPequeno = {10,rand() % 500, 10,10};
     //printf("%d", Rects[0].x);
@@ -128,6 +178,7 @@ int main (int argc, char* args[])
         SDL_Event evt;
         int aux = AUX_WaitEventTimeoutCount(&evt, &espera);
         if (Game == MENU){
+            SDL_ShowCursor(SDL_ENABLE);
             SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
             SDL_RenderClear(ren);
             SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
@@ -138,7 +189,7 @@ int main (int argc, char* args[])
             //SDL_RenderFillRect(ren, &BotaoQuit);
             SDL_RenderPresent(ren);
             switch (evt.type){
-                case SDL_MOUSEBUTTONDOWN:
+                case SDL_MOUSEBUTTONUP:
                     if (evt.button.button == SDL_BUTTON_LEFT){
                         if (PointInRect(&mousePos, &BotaoStart)){
                             Game = RUNNING;
@@ -173,9 +224,46 @@ int main (int argc, char* args[])
             SDL_RenderClear(ren);
             SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
             SDL_RenderCopy(ren, bg, NULL, &fundo);
+
+            sprintf(PontosString, "%d", Points);
+            ScoreRect.w = 30*strlen(PontosString);
+            render_text(ren, PontosString, fnt, &ScoreRect, &clr);
+
+            /*sfc1 = TTF_RenderText_Solid(fnt, PontosString, clr);
+            assert(sfc1 != NULL);
+            score = SDL_CreateTextureFromSurface(ren, sfc1);
+            assert(score != NULL);
+            SDL_FreeSurface(sfc1);*/
+
+            sprintf(comboString, "%d", combocount);
+            render_text(ren, comboString, fnt, &comboRect, &clr);
+            /*sfc2 = TTF_RenderText_Solid(fnt, comboString, clr);
+            assert(sfc2 != NULL);
+            combo = SDL_CreateTextureFromSurface(ren, sfc2);
+            assert(combo != NULL);
+            SDL_FreeSurface(sfc2);*/
+
+            render_text(ren, "x", fnt, &xRect, &clr);
+            /*sfc3 = TTF_RenderText_Solid(fnt, "x", clr);
+            assert(sfc3 !=NULL);
+            texX = SDL_CreateTextureFromSurface(ren, sfc3);
+            assert(texX !=NULL);
+            SDL_FreeSurface(sfc3);*/
+
+            //SDL_RenderCopy(ren,score,NULL,&ScoreRect);
+            //SDL_RenderCopy(ren,combo, NULL,&comboRect);
+            //SDL_RenderCopy(ren,texX, NULL,&xRect);
+            //SDL_DestroyTexture(score);
+           // SDL_DestroyTexture(texX);
+            //SDL_DestroyTexture(combo);*/
             for (i=0; i<entcout; i++) SDL_RenderCopy(ren, ast, NULL, &Rects[i]);//SDL_RenderFillRect(ren, &Rects[i]);
-            SDL_RenderCopy(ren, img, NULL, &Mira);
-            //SDL_RenderFillRect(ren, &Mira);
+            if (Gun==READY || Gun==SHOOTING){
+                recorteMira = (SDL_Rect) {0,0, 512,512};
+            }else {
+                recorteMira = (SDL_Rect) {512,0, 512,512};
+            }
+            SDL_RenderCopy(ren, img, &recorteMira, &Mira);
+            //SDL_RenderFillRect(ren, &Terra);
             SDL_RenderPresent(ren);
        
             //SDL_Event evt;
@@ -189,46 +277,60 @@ int main (int argc, char* args[])
                     Mira.y = my - (Mira.h/2);
                 }
                 if (evt.type == SDL_MOUSEBUTTONDOWN){
-                    if (evt.button.button == SDL_BUTTON_LEFT){
-                        if (Gun == READY){
+                    if (evt.button.button == SDL_BUTTON_LEFT && Gun == READY){
                         Gun = SHOOTING;
-                        }
+                        hit = 0; 
                         for (i=0; i<entcout; i++){
+                            //if (!(SDL_HasIntersection(&Mira, &Rects[i])) && Gun == SHOOTING){
+                            //    combocount = 0;   
+                            //}
                             if (SDL_HasIntersection(&Mira, &Rects[i]) && Gun == SHOOTING){
+                                Gun = RELOADING;
+                                combocount+=1;
+                                hit = 1;
                                 if (Rects[i].h>10){
+                                    Points+=10*combocount;
                                     split (Rects, i, Rects[i], entcout);
-                                    entcout+=3;        
+                                    entcout+=2;        
                                 }
                                 else{
                                     remover_elemento(Rects, i);
                                     entcout-=1;
+                                    Points+=5*combocount;
                                 }          
-                            }
+                            }       
+                        }
+                        if (hit == 0){
+                            combocount = 0;
                         }
                         Gun = RELOADING;
                     }
                 }
+                if (evt.type == SDL_KEYDOWN){
+                    if (evt.key.keysym.sym == SDLK_ESCAPE){
+                        Game = MENU;
+                    }
+                }
             }
-            else {
-                //espera=es;                
+            else {       
+                //espera = es;        
                 //entrespawn++;
                 if (Gun == RELOADING){
                     cooldown++;
-                    if (cooldown==10){
+                    if (cooldown==5){
                         Gun = READY;
                         cooldown=0;
                     }
                 }
                 //espera=es;
                 //entrespawn=0;
-                for (i=0; i<3; i++){
-                    entrespawn= rand() % 100;
-                    if(entrespawn>97 && entcout<10){
-                        spawn(Rects, entcout);
-                        entcout++;
-                    }
+                //for (i=0; i<3; i++){
+                entrespawn= rand() % 100;
+                if(entrespawn>95 && entcout<10){
+                    spawn(Rects, entcout);
+                    entcout++;
                 }
-                espera=es;
+                //}
                 //if (entrespawn==5 && entcout<10){
                 //while (entcout<10){
                 /*    gen = rand() % 2;
@@ -244,17 +346,20 @@ int main (int argc, char* args[])
                     }
                 }*/
                 //}
+                espera=es;
                 for (i=0; i<entcout; i++){
                     if (Rects[i].h==10){          
                         Rects[i].x += 2;
                         Rects[i].y += 2;
                     }
                     if (Rects[i].h>10){
-                        Rects[i].x +=10;
+                        Rects[i].x +=2;
+                        Rects[i].y +=2;
                     }
-                    if (Rects[i].x + Rects[i].w > ww || Rects[i].y + Rects[i].h > wh){
+                    if (Rects[i].x + Rects[i].w > ww || SDL_HasIntersection(&Terra, &Rects[i])){//Rects[i].y + Rects[i].h > wh){
                         remover_elemento(Rects, i);
                         entcout-=1;
+                        combocount = 0;
                     }
                 }
             }
@@ -268,9 +373,19 @@ int main (int argc, char* args[])
     }
 
     /* FINALIZACAO */
+    //SDL_DestroyTexture(score);
+    //SDL_DestroyTexture(texX);
+    //SDL_DestroyTexture(combo);
+    TTF_CloseFont(fnt);
     SDL_DestroyTexture(img);
+    SDL_DestroyTexture(bg);
+    SDL_DestroyTexture(ast);
+    SDL_DestroyTexture(mn);
+    SDL_DestroyTexture(bp);
+    SDL_DestroyTexture(bq);
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
+    TTF_Quit();
     SDL_Quit();
     return 0;
 }
